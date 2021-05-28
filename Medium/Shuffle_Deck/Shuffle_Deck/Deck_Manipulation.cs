e1ae5616e6f011ed02d74d99f795dc0af7962d4c
Benjamin Lafever <52665892+Benjamin-Lafever@users.noreply.github.com>
2021-05-28 14:45:20 -0400

    Add files via upload

---
+++ Medium/Shuffle_Deck/Shuffle_Deck/Deck_Manipulation.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shuffle_Deck
+{
+    
+    class Deck_Manipulation: Random
+    {
+        static List<string> shuffled_Deck = new List<string>();
+        static List<string> deck = new List<string>();
+
+        static StringBuilder sb = new StringBuilder();
+        static Random rand = new Random();
+
+        static string card;
+        static int number;
+        static int random;
+
+        public static List<string> create_Starting_Deck(List<string> faces, List<string> suits)
+        {
+            foreach (string s in suits)
+            {
+                foreach(string s_1 in faces)
+                {
+                    sb.Clear();
+                    sb.Append(s_1);
+                    sb.Append(" of ");
+                    sb.Append(s);
+                    card = sb.ToString();
+                    deck.Add(card);
+                }
+            }
+            return deck;
+        }
+        public static List<string> shuffle_Deck(List<string> unshuffled_Deck)
+        {
+            number = unshuffled_Deck.Count;
+            while(shuffled_Deck.Count < 52)
+            {
+                random = rand.Next(number);
+
+                if (shuffled_Deck.Contains(unshuffled_Deck[random]))
+                {
+                    goto cont;
+                }
+                else
+                {
+                    shuffled_Deck.Add(unshuffled_Deck[random]);
+                }
+                cont:;
+            }
+            return shuffled_Deck;
+        }
+    }
+}
---
+++ Medium/Shuffle_Deck/Shuffle_Deck/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Shuffle_Deck
+{
+    class Program
+    {
+        static List<string> suits = new List<string>{ "Spades", "Hearts", "Diamonds", "Clubs" };
+        static List<string> faces = new List<string>{ "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A" };
+        static List<string> deck = new List<string>();
+
+        static void Main(string[] args)
+        {
+            deck = Deck_Manipulation.create_Starting_Deck(faces, suits);
+            deck.ForEach(Console.WriteLine);
+            Console.WriteLine("\n Press enter to shuffle your deck\n");
+            Console.ReadLine();
+            deck = Deck_Manipulation.shuffle_Deck(deck);
+            deck.ForEach(Console.WriteLine);
+            Console.ReadLine();
+
+        }
+    }
+}
+
+//Good morning! Here's your coding interview problem for today.
+
+//This problem was asked by Facebook.
+
+//Given a function that generates perfectly random numbers between 1 and k (inclusive), where k is an input, write a function that shuffles a deck of cards represented as an array using only swaps.
+
+//It should run in O(N) time.
+
+//Hint: Make sure each one of the 52! permutations of the deck is equally likely.
